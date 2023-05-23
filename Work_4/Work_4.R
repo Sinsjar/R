8a0d6e34b59bd92fda0d901b4367d97af46f4980
Sinsjar <125882945+Sinsjar@users.noreply.github.com>
2023-05-23 12:29:17 +0300

    Практика_4

---
+++ Work_4/Work_4.R
@@ -0,0 +1,15 @@
+
+# Загрузка файла с интернета в папку проекта
+
+#url <- "https://raw.githubusercontent.com/qwerty29544/RpracticeBook/master/2Data/01FlatTables/GAZ.csv"
+
+#destfile <- "C:/Users/Sinsjar/Desktop/R_projects/Work_4"
+
+#download.file(url, destfile="GAZ.csv")
+
+
+# Zadanie_1 ---------------------------------------------------------------
+
+
+library('readxl')
+library('')
